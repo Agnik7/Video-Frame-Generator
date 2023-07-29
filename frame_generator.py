75453a5ae387379c3f1a866c9afab7e31c894418
Agnik Bakshi <bakshi.agnik@gmail.com>
2023-07-29 10:13:51 +0530

    Modularized the code

--- frame_generator.py
+++ frame_generator.py
@@ -1,40 +1,45 @@
 import cv2
 
-# Accessing the camera 
-cap = cv2.VideoCapture(0)
 
-# Defining the codec and creating the VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-file_name = 'output'
-output = cv2.VideoWriter((file_name+'.avi'), fourcc, 24.0, (640,  480))
+def generate_frame():
+    # Accessing the camera 
+    cap = cv2.VideoCapture(0)
 
-# Defining the variables save and c
-save = 'Frame1'
-c = 2;
-while cap.isOpened():
+    # Defining the codec and creating the VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    file_name = 'output'
+    output = cv2.VideoWriter((file_name+'.avi'), fourcc, 24.0, (640,  480))
 
-    res, frame = cap.read()
-    if not res:
-        print("Frame Cannot Be Received")
-        break
-    
-    # Flipping the frame horizontally to get correct orientation
-    frame = cv2.flip(frame, 90)
+    # Defining the variables save and c
+    save = 'Frame1'
+    c = 2;
+    while cap.isOpened():
 
-    # Displaying the current frame
-    output.write(frame)
-    cv2.imshow('Frame', frame)
+        res, frame = cap.read()
+        if not res:
+            print("Frame Cannot Be Received")
+            break
+        
+        # Flipping the frame horizontally to get correct orientation
+        frame = cv2.flip(frame, 90)
 
-    # Saving the frames
-    cv2.imwrite(save+'.png', frame )
-    save = "Frame"+ str(c)
-    c = c+1
+        # Displaying the current frame
+        output.write(frame)
+        cv2.imshow('Frame', frame)
 
-    # If no input is received for 1ms, or if the key 'x' is pressed, interpreter goes outside of the loop
-    if cv2.waitKey(1) == ord('x'):
-        break
+        # Saving the frames
+        cv2.imwrite(save+'.png', frame )
+        save = "Frame"+ str(c)
+        c = c+1
 
-# Releasing everything after coming out of loop
-cap.release()
-output.release()
-cv2.destroyAllWindows()
+        # If no input is received for 1ms, or if the key 'x' is pressed, interpreter goes outside of the loop
+        if cv2.waitKey(1) == ord('x'):
+            break
+
+    # Releasing everything after coming out of loop
+    cap.release()
+    output.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    generate_frame()
